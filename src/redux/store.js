1df528b0c3724ecc17f8a6136736bab0345e6f4b
Shariq <shariqshaikh2705@gmail.com>
2025-09-04 18:11:24 +0530

    restart

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import textStateSlice from "./Reducers/textStateSlice";
-
-export const store = configureStore({
-    reducer: {
-        textState : textStateSlice,
-    },
-})
